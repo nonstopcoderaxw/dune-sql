72f5771cb3dcfaa3a141f6db908fea14ae91777a
axw <axw>
2023-11-01 21:11:05 +1100

    update

---
+++ dune/dashboards/ethereum-traces/transactions-ethereum-table-finder.sql
@@ -0,0 +1,126 @@
+--basing off of https://explorer.phalcon.xyz/tx/eth/0x56aa11363ccf5c67092a5cde4c15f7f42cdfb503b191d1040ff1b9681566126c
+with 
+    calls_base as (
+        SELECT 
+            tr.trace_address
+            , case 
+                when c.namespace is not null then concat(c.namespace, '_', '{{chain}}', '.', c.name, '_call_', split_part(sig.signature,'(',1))
+                when tr.type = 'create' then 'trace_create (' || COALESCE(lower(replace(c.name, ': ', '_{{chain}}.')), cast(tr.address as varchar)) || ')'
+                when bytearray_length(tr.input) = 0 then 'trace_' || tr.call_type --empty input implies just a value transfer or something was used.
+                else 'unknown_call (' || cast(bytearray_substring(tr.input,1,4) as varchar) || ')'
+            end
+            as decoded_table_name
+            , coalesce(sig.signature, '_unknown_') as function_event
+            , COALESCE(tr.to, tr.address) as contract_address
+            , tr.success
+            , tr."from"
+            , bytearray_substring(tr.input,1,4) as signature
+            , json_value(sig.abi, 'strict $.stateMutability') as function_type
+            , call_type
+        FROM {{chain}}.traces tr
+        LEFT JOIN (SELECT *, row_number() over (partition by blockchain, address order by created_at desc) as last_submitted 
+            FROM evms.contracts) c
+            ON blockchain = '{{chain}}' 
+            and (c.address = tr.address OR c.address = tr.to)
+            and last_submitted = 1
+        LEFT JOIN {{chain}}.signatures sig ON bytearray_substring(input,1,4) = sig.id AND sig.name = c.name AND sig.namespace = c.namespace
+        WHERE tr.tx_hash = {{tx hash}}
+        and tr.block_number = {{blocknumber}}
+        -- AND lower(tr.function_name) NOT LIKE '%approve%' --here just to simplify the example case
+        order by trace_address asc
+    )
+
+    , logs_base as (
+        SELECT 
+            case 
+                when c.namespace is not null then concat(c.namespace, '_', '{{chain}}', '.', c.name, '_evt_', split_part(sig.signature,'(',1))
+                else 'unknown_log (' || cast(topic0 as varchar) || ')'
+            end
+            as decoded_table_name
+            , coalesce(sig.signature, '_unknown_') as function_event
+            , row_number() over (partition by contract_address order by index asc) as event_log_count
+            , contract_address
+            , index
+            , topic0 as signature
+        FROM {{chain}}.logs l
+        LEFT JOIN (SELECT *, row_number() over (partition by blockchain, address order by created_at desc) as last_submitted 
+            FROM evms.contracts) c
+            ON blockchain = '{{chain}}' 
+            and c.address = l.contract_address
+            and last_submitted = 1
+        LEFT JOIN {{chain}}.signatures sig ON topic0 = sig.id AND sig.name = c.name AND sig.namespace = c.namespace
+        WHERE l.tx_hash = {{tx hash}}
+        and l.block_number = {{blocknumber}}
+        order by index asc
+    )
+    
+    , logs_ordered as (
+        SELECT 
+            l.decoded_table_name
+            , l.function_event
+            , get_href(get_chain_explorer_address('{{chain}}', l.contract_address), cast(l.contract_address as varchar)) as contract_address
+            , null as "from"
+            , l.signature
+            , true as success
+            , coalesce(trace_address, lag(trace_address, 1) IGNORE NULLS OVER (PARTITION BY 1 ORDER BY l.index asc)) as trace_address --|| 9999 can be added to place the event at the very end of the trace.
+            , l.index as evt_index
+        FROM logs_base l 
+        LEFT JOIN (SELECT 
+                    *
+                    , row_number() over (partition by contract_address order by trace_address asc) as contract_call_count
+                FROM calls_base
+                WHERE function_type not in ('view','pure')
+                and call_type not in ('staticcall', 'delegatecall')
+            ) c
+            ON c.contract_address = l.contract_address AND c.contract_call_count = l.event_log_count
+        order by l.index asc, trace_address asc
+    )
+
+SELECT
+*
+FROM (
+SELECT 
+    array_join(repeat('&nbsp&nbsp', cast(cardinality(trace_address) as int)) , '') 
+    || cast(cardinality(trace_address) as varchar) 
+    || ' -> 🟢 '
+    || call_type
+        as tx_type
+    , decoded_table_name
+    , function_event
+    , get_href(get_chain_explorer_address('{{chain}}', contract_address),cast(contract_address as varchar)) as contract_address
+    , "from"
+    , signature
+    , success
+    , trace_address
+    , null as evt_index
+FROM calls_base
+UNION ALL 
+SELECT
+    array_join(repeat('&nbsp&nbsp', cast(cardinality(trace_address) as int)) , '') 
+    || '&nbsp&nbsp-> 🟠 event'
+    as tx_type
+    , *
+FROM logs_ordered
+)
+WHERE ('{{ignore unknown}}' = 'false' OR decoded_table_name not like '%unknown_%')
+order by trace_address asc, evt_index asc nulls first
+
+--, suggested table to use, could be much more built out since this is just a showcase
+-- , case when tr.function_name LIKE '%transfer%' OR bytearray_substring(tr.input,1,4) IN (0xa9059cbb, 0x23b872dd, 0x36c78516) then 'erc20_{{chain}}.evt_Transfer'
+--     when lower(tr.function_name) LIKE '%approve%' then 'erc20_{{chain}}.evt_Approval'
+--     when lower(tr.function_name) LIKE '%swap%' AND tr.namespace IN ('oneinch','zeroex','cowprotocol','paraswap') then 'dex_aggregator.trades'
+--     when lower(tr.function_name) LIKE '%swap%' AND tr.namespace NOT IN ('oneinch','zeroex','cowprotocol','paraswap') then 'dex.trades'
+--     when lower(tr.function_name) LIKE '%flashloan%' AND tr.namespace LIKE '%aave%' then 'aave.flashloans' 
+--     end as spellbook_table_name
+-- , reduce(
+--       cast(json_parse(json_query(s.abi,'lax $.inputs[*]' with array wrapper)) as array(row(name varchar, type varchar))),
+--       '',
+--       (acc, elem) -> acc || '<b>' || elem.name || '</b>::' || elem.type || ', ',
+--       acc -> rtrim(acc, ', ')
+--      ) as inputs
+-- , reduce(
+--       cast(json_parse(json_query(s.abi,'lax $.outputs[*]' with array wrapper)) as array(row(name varchar, type varchar))),
+--       '',
+--       (acc, elem) -> acc || '<b>' || elem.name || '</b>::' || elem.type || ', ',
+--       acc -> rtrim(acc, ', ')
+--      ) as outputs
---
+++ dune/postgres_scripts/create_tables.sql
@@ -0,0 +1,228 @@
+-- STEP 1
+DROP TABLE if exists "cex.addresses";
+
+DROP TABLE if exists "ethereum.traces";
+
+DROP TABLE if exists "ethereum.transactions";
+
+DROP TABLE if exists "evms.contracts";
+
+DROP TABLE if exists "ethereum.signatures";
+
+DROP TABLE if exists "ethereum.logs";
+
+-- STEP 2
+CREATE TABLE "cex.addresses"(
+    blockchain TEXT,
+    address TEXT,
+    cex_name TEXT,
+    distinct_name TEXT,
+    added_by TEXT,
+    added_date date
+);
+
+CREATE TABLE "ethereum.traces"(
+    block_time timestamp,
+    block_number int8,
+    value int8,
+    gas int8,
+    gas_used int8,
+    block_hash TEXT,
+    success bool,
+    tx_index int8,
+    sub_traces int8,
+    error TEXT,
+    tx_success bool,
+    tx_hash TEXT,
+    "from" TEXT,
+    "to" TEXT,
+    trace_address TEXT,
+    type TEXT,
+    address TEXT,
+    code TEXT,
+    call_type TEXT,
+    input TEXT,
+    output TEXT,
+    refund_address TEXT,
+    block_date date
+);
+
+CREATE TABLE "ethereum.transactions"(
+    block_time timestamp,
+    block_number int8,
+    value int8,
+    gas_limit int8,
+    gas_price int8,
+    gas_used int8,
+    max_fee_per_gas int8,
+    max_priority_fee_per_gas int8,
+    priority_fee_per_gas int8,
+    nonce int8,
+    index int8,
+    success bool,
+    "from" TEXT,
+    "to" TEXT,
+    block_hash TEXT,
+    data TEXT,
+    hash TEXT,
+    type TEXT,
+    access_list TEXT,
+    block_date date
+);
+
+CREATE TABLE "evms.contracts"(
+    blockchain TEXT,
+    abi TEXT,
+    address TEXT,
+    "from" TEXT,
+    code TEXT,
+    name TEXT,
+    namespace TEXT,
+    dynamic TEXT,
+    base TEXT,
+    factory TEXT,
+    detection_source TEXT,
+    created_at TEXT
+);
+
+CREATE TABLE "ethereum.signatures"(
+    id TEXT,
+    signature TEXT,
+    abi TEXT,
+    type TEXT,
+    namespace TEXT,
+    name TEXT,
+    created_at date
+);
+
+CREATE TABLE "ethereum.logs"(
+    block_time timestamp,
+    block_number int8,
+    block_hash TEXT,
+    contract_address TEXT,
+    topic0 TEXT,
+    topic1 TEXT,
+    topic2 TEXT,
+    topic3 TEXT,
+    data TEXT,
+    tx_hash TEXT,
+    index int8,
+    tx_index int8,
+    block_date date,
+    tx_from TEXT,
+    tx_to TEXT
+);
+
+-- STEP 3
+COPY "cex.addresses"(
+    blockchain,
+    address,
+    cex_name,
+    distinct_name,
+    added_by,
+    added_date
+)
+FROM
+    '/Volumes/t7/mac/gitProjects/dune-sql/dune/postgres_scripts/csv/cex.addresses.csv' DELIMITER ',' CSV HEADER;
+
+COPY "ethereum.traces"(
+    block_time,
+    block_number,
+    value,
+    gas,
+    gas_used,
+    block_hash,
+    success,
+    tx_index,
+    sub_traces,
+    error,
+    tx_success,
+    tx_hash,
+    "from",
+    "to",
+    trace_address,
+    type,
+    address,
+    code,
+    call_type,
+    input,
+    output,
+    refund_address,
+    block_date
+)
+FROM
+    '/Volumes/t7/mac/gitProjects/dune-sql/dune/postgres_scripts/csv/ethereum.traces.csv' DELIMITER ',' CSV HEADER;
+
+COPY "ethereum.transactions"(
+    block_time,
+    block_number,
+    value,
+    gas_limit,
+    gas_price,
+    gas_used,
+    max_fee_per_gas,
+    max_priority_fee_per_gas,
+    priority_fee_per_gas,
+    nonce,
+    index,
+    success,
+    "from",
+    "to",
+    block_hash,
+    data,
+    hash,
+    type,
+    access_list,
+    block_date
+)
+FROM
+    '/Volumes/t7/mac/gitProjects/dune-sql/dune/postgres_scripts/csv/ethereum.transactions.csv' DELIMITER ',' CSV HEADER;
+
+COPY "evms.contracts"(
+    blockchain,
+    abi,
+    address,
+    "from",
+    code,
+    name,
+    namespace,
+    dynamic,
+    base,
+    factory,
+    detection_source,
+    created_at
+)
+FROM
+    '/Volumes/t7/mac/gitProjects/dune-sql/dune/postgres_scripts/csv/evms.contracts.csv' DELIMITER ',' CSV HEADER;
+
+COPY "ethereum.signatures"(
+    id,
+    signature,
+    abi,
+    type,
+    namespace,
+    name,
+    created_at
+)
+FROM
+    '/Volumes/t7/mac/gitProjects/dune-sql/dune/postgres_scripts/csv/ethereum.signatures.csv' DELIMITER ',' CSV HEADER;
+
+COPY "ethereum.logs"(
+    block_time,
+    block_number,
+    block_hash,
+    contract_address,
+    topic0,
+    topic1,
+    topic2,
+    topic3,
+    data,
+    tx_hash,
+    index,
+    tx_index,
+    block_date,
+    tx_from,
+    tx_to
+)
+FROM
+    '/Volumes/t7/mac/gitProjects/dune-sql/dune/postgres_scripts/csv/ethereum.logs.csv' DELIMITER ',' CSV HEADER;
